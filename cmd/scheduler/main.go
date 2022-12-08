e0ce7fc86b442337bca29c09a756a5fc35e9cbcc
H.J <jeff.he@outlook.com>
2022-12-08 12:35:26 +0000

    merged the latest fix
    Signed-off-by: H.J <jeff.he@outlook.com>

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -1,22 +1,32 @@
 package main
 
 import (
+	"eswarm-scheduler/pkg/eswarm"
+	//"eswarm-scheduler/pkg/register"
+	//"flag"
 	"fmt"
-	"github.com/hejin/eswarm-scheduler/pkg/register"
+	//"k8s.io/component-base/cli"
 	"k8s.io/component-base/logs"
+	"k8s.io/klog"
+	"k8s.io/kubernetes/cmd/kube-scheduler/app"
 	"math/rand"
 	"os"
-	"pkg/register"
+	_ "sigs.k8s.io/scheduler-plugins/apis/config/scheme"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
-	command := register.Register()
+	//command := register.Register()
+	command := app.NewSchedulerCommand(app.WithPlugin(eswarm.Name, eswarm.New))
 	logs.InitLogs()
+	//flag.Parse()
 	defer logs.FlushLogs()
+
+	klog.Infof("Welcome to eSwarm!!!")
 	if err := command.Execute(); err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
+		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
+	klog.Infof("Bye from eSwarm...")
 }
--- pkg/eswarm/scheduler.go
+++ pkg/eswarm/scheduler.go
@@ -2,11 +2,19 @@ package eswarm
 
 import (
 	"context"
+	"errors"
+	//"fmt"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/klog/v2"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
+	//"k8s.io/apimachinery/pkg/types"
+	//scv "github.com/NJUPT-ISL/SCV/api/v1"
+	"k8s.io/klog"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 )
 
 const (
@@ -14,17 +22,62 @@ const (
 	Name = "eswarm"
 )
 
-var _ framework.FilterPlugin = &eSwarm{}
-var _ framework.PreBindPlugin = &eSwarm{}
+var (
+	_ framework.FilterPlugin  = &eSwarm{}
+	_ framework.PreBindPlugin = &eSwarm{}
+	// GroupVersion is group version used to register these objects
+	GroupVersion = schema.GroupVersion{Group: "core.run-linux.com", Version: "v1"}
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+
+	// AddToScheme adds the types in this group-version to the given scheme.
+	AddToScheme = SchemeBuilder.AddToScheme
+	schemeLocal = runtime.NewScheme()
+)
 
 type eSwarm struct {
 	handle framework.Handle
+	cache  cache.Cache
 }
 
 func New(_ runtime.Object, handle framework.Handle) (framework.Plugin, error) {
-	return &eSwarm{
-		handle: handle,
-	}, nil
+	mgrConfig := ctrl.GetConfigOrDie()
+	mgrConfig.QPS = 1000
+	mgrConfig.Burst = 1000
+
+	//if err := scv.AddToScheme(schemeLocal); err != nil {
+	if err := AddToScheme(schemeLocal); err != nil {
+		klog.Error(err)
+		return nil, err
+	}
+
+	mgr, err := ctrl.NewManager(mgrConfig, ctrl.Options{
+		Scheme:             schemeLocal,
+		MetricsBindAddress: "",
+		LeaderElection:     false,
+		Port:               9443,
+	})
+	if err != nil {
+		klog.Error(err)
+		return nil, err
+	}
+	go func() {
+		if err = mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+			klog.Error(err)
+			panic(err)
+		}
+	}()
+	scvCache := mgr.GetCache()
+
+	if scvCache.WaitForCacheSync(context.TODO()) {
+		return &eSwarm{
+			handle: handle,
+			cache:  scvCache,
+		}, nil
+	} else {
+		return nil, errors.New("Cache Not Sync! ")
+	}
 }
 
 func (s *eSwarm) Name() string {
@@ -32,7 +85,7 @@ func (s *eSwarm) Name() string {
 }
 
 func (s *eSwarm) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, node *framework.NodeInfo) *framework.Status {
-	klog.V(2).Infof("eswarm --> filter pod: %v", pod.Name)
+	klog.Infof("eswarm --> filter pod: %v", pod.Name)
 	return framework.NewStatus(framework.Success, "")
 }
 
@@ -41,6 +94,6 @@ func (s *eSwarm) PreBind(ctx context.Context, state *framework.CycleState, pod *
 	if err != nil {
 		return framework.NewStatus(framework.Error, err.Error())
 	}
-	klog.V(2).Infof("eswarm --> prebind node info: %+v", nodeInfo.Node())
+	klog.Infof("eswarm --> prebind node info: %+v", nodeInfo.Node())
 	return framework.NewStatus(framework.Success, "")
 }
