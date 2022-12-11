92891e0382f3ee2eb23d825e0237717d4e43474a
H.J <jeff.he@outlook.com>
2022-12-11 06:11:15 +0000

    code cleanup further
    Signed-off-by: H.J <jeff.he@outlook.com>

--- pkg/eswarm/scheduler.go
+++ pkg/eswarm/scheduler.go
@@ -2,16 +2,11 @@ package eswarm
 
 import (
 	"context"
-	"errors"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/klog"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/cache"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
 const (
@@ -22,62 +17,19 @@ const (
 var (
 	_ framework.FilterPlugin  = &eSwarm{}
 	_ framework.PreBindPlugin = &eSwarm{}
-	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "core.run-linux.com", Version: "v1"}
-
-	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
-
-	// AddToScheme adds the types in this group-version to the given scheme.
-	AddToScheme = SchemeBuilder.AddToScheme
-	schemeLocal = runtime.NewScheme()
 )
 
 type eSwarm struct {
 	handle framework.Handle
-	cache  cache.Cache
-}
-
-func New(_ runtime.Object, handle framework.Handle) (framework.Plugin, error) {
-	mgrConfig := ctrl.GetConfigOrDie()
-	mgrConfig.QPS = 1000
-	mgrConfig.Burst = 1000
-
-	if err := AddToScheme(schemeLocal); err != nil {
-		klog.Error(err)
-		return nil, err
-	}
-
-	mgr, err := ctrl.NewManager(mgrConfig, ctrl.Options{
-		Scheme:             schemeLocal,
-		MetricsBindAddress: "",
-		LeaderElection:     false,
-		Port:               9443,
-	})
-	if err != nil {
-		klog.Error(err)
-		return nil, err
-	}
-	go func() {
-		if err = mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-			klog.Error(err)
-			panic(err)
-		}
-	}()
-	Cache := mgr.GetCache()
-
-	if Cache.WaitForCacheSync(context.TODO()) {
-		return &eSwarm{
-			handle: handle,
-			cache:  Cache,
-		}, nil
-	} else {
-		return nil, errors.New("Cache Not Sync! ")
-	}
 }
 
 func (s *eSwarm) Name() string {
 	return Name
+}
+func New(_ runtime.Object, handle framework.Handle) (framework.Plugin, error) {
+	return &eSwarm{
+		handle: handle,
+	}, nil
 }
 
 func (s *eSwarm) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, node *framework.NodeInfo) *framework.Status {
